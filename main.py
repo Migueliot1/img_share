20df0c051b305a2093e0c02ae1f9f6567604fdf7
Migueliot1 <migueliot@protonmail.com>
2022-03-27 03:33:57 +0300

    Create empty download_img() function.

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.lang import Builder
+import wikipedia
+import requests
 
 Builder.load_file('frontend.kv')
 
@@ -9,6 +11,8 @@ class FirstScreen(Screen):
     def search_img(self):
         self.manager.current_screen.ids.showed_img.source = 'files/tomb.png'
 
+    def download_img(self, name):
+        pass
 
 class RootWidget(ScreenManager):
     pass
